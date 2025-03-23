06cd5de9d8c46b52a46c5c11203523c2570d4aac
Gorenburg Stanislav <stas.gorenburg@gmail.com>
2025-03-23 02:16:53 +0200

    first commit

---
+++ app.ts
@@ -0,0 +1,29 @@
+import { Bot as GrammyBot, GrammyError, HttpError } from "grammy";
+
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const bot = new GrammyBot(process.env.BOT_API_TOKEN as string)
+
+bot.api.setMyCommands([
+
+])
+
+bot.catch((err) => { //err
+    const ctx = err.ctx
+    console.log(`Error while handling update ${ctx.update.update_id}`)
+    const e = err.error
+
+    if( e instanceof GrammyError){
+        console.log('Error in request:', e.description)
+    }
+    else if(e instanceof HttpError){
+        console.log('Error in request', e)
+    }
+    else {
+        console.log('Error undefined', e)
+    }
+})
+
+bot.start()
